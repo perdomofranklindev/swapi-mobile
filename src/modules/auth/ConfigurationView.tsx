c9b44d4f5cedfcd85a84bb9208560a26a0bf39e6
perdomofranklindev <perdomofranklindev@gmail.com>
2023-01-22 22:03:37 -0400

    Nice header

--- src/modules/auth/ConfigurationView.tsx
+++ src/modules/auth/ConfigurationView.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { View, Heading, Button } from 'native-base';
+import { View, Button, Container, Center, Heading } from 'native-base';
 import { useSessionStore } from './auth-store';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { BottomTabParamList } from '../navigation/navigation-types';
-
 type ConfigurationProps = NativeStackScreenProps<
   BottomTabParamList,
   'Configuration'
@@ -14,16 +13,20 @@ export const ConfigurationView: React.FC<ConfigurationProps> = ({
   const { clearSession } = useSessionStore();
 
   return (
-    <View>
-      <Heading>Configuration</Heading>
-      <Button
-        color="red.400"
-        onPress={() => {
-          clearSession();
-          navigation.popToTop();
-        }}>
-        Log out
-      </Button>
+    <View flex={1} _light={{ bg: 'white' }} w="100%">
+      <Center>
+        <Container py={5} w="100%">
+          <Button
+            w="100%"
+            color="red.400"
+            onPress={() => {
+              clearSession();
+              navigation.popToTop();
+            }}>
+            Log out
+          </Button>
+        </Container>
+      </Center>
     </View>
   );
 };
--- src/modules/navigation/AppContainer.tsx
+++ src/modules/navigation/AppContainer.tsx
@@ -2,33 +2,59 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { LoginView } from '../auth/LoginView';
 import { useSessionStore } from '../auth/auth-store';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { ConfigurationView } from '../auth/ConfigurationView';
 import { LoadingOverlap } from '../../shared/components/LoadingOverlap';
 import { PeopleView } from '../people/PeopleView';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { PersonCreateView } from '../people/PersonCreateView';
 import {
   BottomTabParamList,
   PeopleStackParamList,
   RootStackParamList,
 } from './navigation-types';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
-const TabNavigator = createMaterialBottomTabNavigator<BottomTabParamList>();
+const TabNavigator = createBottomTabNavigator<BottomTabParamList>();
 const PeopleStack = createNativeStackNavigator<PeopleStackParamList>();
+
+const headerStyles = {
+  headerStyle: {
+    backgroundColor: '#253681',
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+};
 
 const Tab = () => {
   return (
-    <TabNavigator.Navigator initialRouteName="Home">
+    <TabNavigator.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarActiveTintColor: 'blue',
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: {
+          paddingBottom: 4,
+          paddingTop: 4,
+        },
+      }}>
       <TabNavigator.Screen
         name="Home"
         component={People}
-        options={{ title: 'Home' }}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => <Ionicons name="home" size={20} />,
+        }}
       />
       <TabNavigator.Screen
         name="Configuration"
         component={ConfigurationView}
-        options={{ title: 'Configuration' }}
+        options={{
+          ...(headerStyles as any),
+          tabBarIcon: () => <Ionicons name="settings" size={20} />,
+        }}
       />
     </TabNavigator.Navigator>
   );
@@ -40,10 +66,23 @@ const People = () => (
       name="People"
       component={PeopleView}
       options={{
-        headerShown: false,
+        ...(headerStyles as any),
+        title: 'Home',
       }}
     />
-    <PeopleStack.Screen name="PersonCreate" component={PersonCreateView} />
+    <PeopleStack.Group
+      screenOptions={{
+        presentation: 'modal',
+      }}>
+      <PeopleStack.Screen
+        name="PersonCreate"
+        component={PersonCreateView}
+        options={{
+          ...(headerStyles as any),
+          title: 'Create',
+        }}
+      />
+    </PeopleStack.Group>
   </PeopleStack.Navigator>
 );
 
@@ -72,7 +111,15 @@ export const AppContainer = () => {
           }}
         />
       )}
-      {session && _hasHydrated && <Stack.Screen name="Main" component={Tab} />}
+      {session && _hasHydrated && (
+        <Stack.Screen
+          name="Main"
+          component={Tab}
+          options={{
+            headerShown: false,
+          }}
+        />
+      )}
     </Stack.Navigator>
   );
 };
